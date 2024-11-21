a1e343b813977dec66a871290d80d888f56b32d7
adrprates <135389147+adrprates@users.noreply.github.com>
2024-11-21 20:35:47 -0300

    Add files via upload

---
+++ agenda/modelo/contato_class.php
@@ -0,0 +1,47 @@
+<?php
+class Contato
+{
+	//classe entidade	
+	private $id;
+	private $nome;
+	private $email;
+	private $telefone;
+
+	public function __construct() {}
+	public function setId($id)
+	{
+		$this->id = $id;
+	}
+	public function getId()
+	{
+		return $this->id;
+	}
+	//demais getters e setters
+
+	public function getNome()
+	{
+		return $this->nome;
+	}
+	public function setNome($n)
+	{
+		$this->nome = $n;
+	}
+
+	public function getEmail()
+	{
+		return $this->email;
+	}
+	public function setEmail($e)
+	{
+		$this->email = $e;
+	}
+
+	public function getTelefone()
+	{
+		return $this->telefone;
+	}
+	public function setTelefone($t)
+	{
+		$this->telefone = $t;
+	}
+}
